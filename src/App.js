5148d030aafb20fb9b596a4f751e196f6f1e204d
Andrew <andrew.liu01@libertymutual.com>
2018-12-11 13:57:33 -0500

    Get account and show peeps

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+  constructor (props) {
+    super(props);
+    this.state = { peeps: [] };
+  }
+
+  componentDidMount () {
+    axios.get('https://floating-thicket-27491.herokuapp.com/accounts/HjupDnysu')
+      .then(response => {
+        this.setState({
+          peeps: response.data.peeps,
+        }, () => {
+          console.log(JSON.stringify(this.state.peeps, null, 2));
+        });
+      }).catch(error => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <div className="App">
+        <ul>
+          {this.state.peeps.map(peep => <li key={peep.peepId}>{peep.peepId}</li>)}
+        </ul>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
