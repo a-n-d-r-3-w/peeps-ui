d2048e4d239676488d77551cccf6050358c683ac
Andrew <andrew.liu01@libertymutual.com>
2019-01-11 15:48:48 -0500

    Revert "Delete peep"
    This reverts commit 1d0281046c9251018b7a0f0347232e906975d7f0.

--- src/Peep.js
+++ src/Peep.js
@@ -2,13 +2,7 @@ import React, {Component, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import './App.css';
-import {
-  setAccountId,
-  setPeepId,
-  getPeep,
-  updatePeep,
-  deletePeep
-} from './actions';
+import {setAccountId, setPeepId, getPeep, updatePeep} from './actions';
 
 class Peep extends Component {
   constructor(props) {
@@ -89,13 +83,6 @@ class Peep extends Component {
               ref={this.textarea}
             />
           </div>
-          <button
-            type='submit'
-            className="btn btn-danger"
-            onClick={this.props.deletePeep}
-          >
-            Delete
-          </button>
         </form>
       </Fragment>
     );
@@ -131,7 +118,6 @@ const mapDispatchToProps = dispatch => ({
   setPeepId: accountId => dispatch(setPeepId(accountId)),
   getPeep: () => dispatch(getPeep()),
   updatePeep: peepInfo => dispatch(updatePeep(peepInfo)),
-  deletePeep: () => dispatch(deletePeep()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Peep);
